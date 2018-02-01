6adc8047681a8996874ca12aaacd3828144095ab
psy-phy-owner <psyphyowner@gmail.com>
2018-02-01 10:07:26 +0900

    create cgi-bin

---
+++ cgi-bin/debug.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# pylint: disable=invalid-name
+"""Debug"""
+import random
+import cgitb
+cgitb.enable()
+
+print('Content-Type: text/html')
+print('')
+
+no = ramdom.randint(1, 6)
+
+print(no)
